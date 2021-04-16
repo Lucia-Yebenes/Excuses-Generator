af5879190d267dbbb11cab205fd7d8327d42c319
Lucia Yebenes <yebeneslucia@gmail.com>
2021-04-16 14:24:59 +0000

    Ej terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let NoRandomWho = who[Math.floor(Math.random() * 4)];
+  let NoRandmAction = action[Math.floor(Math.random() * 4)];
+  let NoRandmWhat = what[Math.floor(Math.random() * 3)];
+  let NoRandmWhen = when[Math.floor(Math.random() * 4)];
+  let Excusa;
+  function GeneradorExcusas() {
+    Excusa =
+      NoRandomWho + " " + NoRandmAction + " " + NoRandmWhat + " " + NoRandmWhen;
+    return Excusa;
+  }
+  console.log(GeneradorExcusas());
+  //Si fuera solo hasta excusas me trae desde html a java con el .InnerHTML paso desde jS a HTML
+  document.getElementById("Excusa").innerHTML = Excusa;
+  //para traer de html a js, importante entre parentesis se pone el nombre de la clase o el ide
+  //si usas ElementsById
+  //   console.log(document.getElementsByClassName("alert"));
 };
+
+// mi codigo, extrategia creo variables y despues concateno
+
+//
